300c2fda55edbc0563393bf593146c2cb467cf33
Karla Solorzano <karli.02@hotmail.com>
2023-01-27 10:13:56 -0600

    work in progress

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+  buscar(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe({ 
+        next: ((paises) => {
+          console.log(paises);
+          this.paises = paises;
+        }),
+        error: ((err) => {
+          this.hayError = true;
+          this.paises = [];
+          })
+      });
+  }
+
+  sugerencias(termino: string){
+    this.hayError = false;
+  }
+}
---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `]
+})
+export class PorRegionComponent {
+
+  regiones: string[] = ["EU","EFTA","CARICOM","PA","AU","USAN","EEU","AL","ASEAN","CAIS","CEFTA","NAFTA","SAARC"];
+  regionActiva: string = "";
+  paises:Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region:string):string {
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
+
+    //TODO: hacer el llamado al servicio
+  }
+
+}
